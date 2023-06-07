9e3dc17d9dc062a0ddb84d3c7067e4d096fe1ff0
Daniel Pyon <pyondohyun@gmail.com>
2023-06-06 18:29:01 -0700

    fix signed bug

--- 21.go
+++ 21.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	mt := &MT19937{}
-	mt.Init(1131464071)
+	mt.Init(19650218)
 	x, err := mt.Rand()
 
 	if err != nil {
--- cryptopals.go
+++ cryptopals.go
@@ -485,55 +485,54 @@ func GenerateAesKey() ([]byte, error) {
 //////////////// MT19937-32 \\\\\\\\\\\\\\\\
 
 const (
-	MT19937_W int32 = 32
-	MT19937_N int32 = 624
-	MT19937_M int32 = 397
-	MT19937_R int32 = 31
-	MT19937_A int32 = -1727483681 // 0x9908b0df
-	MT19937_U int32 = 11
-	MT19937_D int32 = -1 // 0xffffffff
-	MT19937_S int32 = 7
-	MT19937_B int32 = -1658038656 // 0x9d2c5680
-	MT19937_T int32 = 15
-	MT19937_C int32 = -272236544 // 0xefc60000
-	MT19937_L int32 = 18
-	MT19937_F int32 = 1812433253
+	MT19937_W uint32 = 32
+	MT19937_N uint32 = 624
+	MT19937_M uint32 = 397
+	MT19937_R uint32 = 31
+	MT19937_A uint32 = 0x9908b0df
+	MT19937_U uint32 = 11
+	MT19937_D uint32 = 0xffffffff
+	MT19937_S uint32 = 7
+	MT19937_B uint32 = 0x9d2c5680
+	MT19937_T uint32 = 15
+	MT19937_C uint32 = 0xefc60000
+	MT19937_L uint32 = 18
+	MT19937_F uint32 = 1812433253
 
-	MT19937_LOWER_MASK int32 = (1 << MT19937_R) - 1
-	MT19937_UPPER_MASK int32 = (^MT19937_LOWER_MASK)
+	MT19937_LOWER_MASK uint32 = (1 << MT19937_R) - 1
+	MT19937_UPPER_MASK uint32 = (^MT19937_LOWER_MASK)
 )
 
 type MT19937 struct {
-	MT []int32
-	index int32
+	MT []uint32
+	index uint32
 }
 
-func (mt *MT19937) Init(seed int32) {
+func (mt *MT19937) Init(seed uint32) {
 	mt.index = MT19937_N
 
-	mt.MT = make([]int32, MT19937_N)
+	mt.MT = make([]uint32, MT19937_N)
 	mt.MT[0] = seed
 
-
-	for i := int32(1); i < MT19937_N; i++ {
-		mt.MT[i] = (MT19937_F * (mt.MT[i - 1] ^ (mt.MT[i - 1] >> (MT19937_W - 2))) + i)
+	for i := uint32(1); i < MT19937_N; i++ {
+		mt.MT[i] = (MT19937_F * (mt.MT[i - 1] ^ (mt.MT[i - 1] >> (MT19937_W - 2)))) + i
 	}
 }
 
 // should not be public
 func (mt *MT19937) twist() {
-	for i := int32(0); i < MT19937_N; i++ {
+	for i := uint32(0); i < MT19937_N; i++ {
 		x := (mt.MT[i] & MT19937_UPPER_MASK) | (mt.MT[(i+1)%MT19937_N] & MT19937_LOWER_MASK)
 		xA := x >> 1
 		if x % 2 != 0 {
-			xA = xA ^ MT19937_A
+			xA ^= MT19937_A
 		}
 		mt.MT[i] = mt.MT[(i+MT19937_M)%MT19937_N] ^ xA
 	}
 	mt.index = 0
 }
 
-func (mt *MT19937) Rand() (int32, error) {
+func (mt *MT19937) Rand() (uint32, error) {
 	if mt.index >= MT19937_N {
 		if mt.index > MT19937_N {
 			return 0, errors.New("generator was never seeded")
@@ -542,10 +541,10 @@ func (mt *MT19937) Rand() (int32, error) {
 	}
 
 	y := mt.MT[mt.index]
-	y ^= (y >> MT19937_U) & MT19937_D
-	y ^= (y << MT19937_S) & MT19937_B
-	y ^= (y << MT19937_T) & MT19937_C
-	y ^= y >> MT19937_L
+	y ^= ((y >> MT19937_U) & MT19937_D)
+	y ^= ((y << MT19937_S) & MT19937_B)
+	y ^= ((y << MT19937_T) & MT19937_C)
+	y ^= (y >> MT19937_L)
 
 	mt.index++
 	return y, nil
