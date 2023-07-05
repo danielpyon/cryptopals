9e457847bf217181116c89dd1cff6aca3d0db493
Daniel Pyon <pyondohyun@gmail.com>
2023-07-04 20:43:58 -0700

    start 29

--- set4/29.go
+++ set4/29.go
@@ -1,1 +1,73 @@
 package set4
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+
+	"github.com/danielpyon/cryptopals/lib/sha1"
+)
+
+// we need to know the length of the original message because that's part of
+// the internal state of sha1 (struct digest in sha1.go)
+
+// tag := SHA-1(key || original-message || new-message)
+
+// Returns whether the sum is a valid MAC tag for the message
+func Sha1Validate(key, message, sum []byte) bool {
+	tmp := sha1.Sum(append(key, message...))
+
+	for i, _ := range sum {
+		if tmp[i] != sum[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Returns SHA1 padded message
+// The length is padded with zeros until it's congruent to 448 mod 512 bits
+// Then the last 64 bits is the length of the message
+func Sha1Padding(data []byte) []byte {
+	bytesNeeded := len(data) % 64
+	if bytesNeeded > 56 {
+		bytesNeeded += 8
+	}
+	bytesNeeded = (56 - bytesNeeded) % 64
+
+	zeros := bytes.Repeat([]byte("\x00"), bytesNeeded)
+	padded := append(data, zeros...)
+	fmt.Println(padded)
+
+	return nil
+}
+
+// Recover the internal state from a checksum
+func Sha1RecoverState(sum [sha1.Size]byte) [5]uint32 {
+	return nil
+}
+
+// Returns a forged SHA1 MAC tag for a given message and extension
+func Sha1LengthExtension(sum [sha1.Size]byte, message, extension, key []byte, length uint64) ([]byte, error) {
+	// the registers used in sha1
+	state := Sha1RecoverState(sum)
+
+	for keyLen := 0; keyLen <= 32; keyLen++ {
+		newMessage := bytes.Repeat([]byte("A"), keyLen)
+		newMessage = Sha1Padding(append(newMessage, message...))
+
+		h := sha1.NewExtension(state, uint64(len(newMessage)))
+		n, err := h.Write(extension)
+		if err != nil || n != len(extension) {
+			return nil, err
+		}
+
+		newSum := h.Sum(nil)
+		if Sha1Validate(key, newMessage[keyLen:], newSum) {
+			return newSum, nil
+		}
+	}
+
+	return nil, errors.New("Could not forge tag!")
+}
--- set4/29_test.go
+++ set4/29_test.go
@@ -1,9 +1,20 @@
 package set4
 
 import (
+	"fmt"
 	"testing"
 )
 
 func Test29(t *testing.T) {
+	x := 56
+	x %= 64
+	if x > 56 {
+		x += 8
+	}
+	bn := (56 - x) % 64
 
+	if bn < 0 {
+		panic("fail")
+	}
+	fmt.Println(bn)
 }
